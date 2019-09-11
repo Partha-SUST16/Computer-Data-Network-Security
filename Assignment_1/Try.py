a334a2e9752c9fdc1b244b35a7d27cb6f9188ce6
Partha Protim Paul <41873352+Partha-SUST16@users.noreply.github.com>
2019-09-11 23:42:51 +0600

    Assignment One
    Substitution Cipher Code in Python using Fitness Score

---
+++ Assignment_1/Try.py
@@ -0,0 +1,44 @@
+from pycipher import SimpleSubstitution as SimpleSub
+import random
+import re
+from ngram_score import ngram_score
+fitness = ngram_score('quadgrams.txt') # load our quadgram statistics
+
+ctext = 'aceah toz puvg vcdl omj puvg yudqecov, omj loj auum klu thmjuv hs klu zlcvu shv zcbkg guovz, upuv zcmdu lcz vuwovroaeu jczoyyuovomdu omj qmubyudkuj vukqvm. klu vcdluz lu loj avhqnlk aodr svhw lcz kvopuez loj mht audhwu o ehdoe eunumj, omj ck toz yhyqeoveg auecupuj, tlokupuv klu hej sher wcnlk zog, klok klu lcee ok aon umj toz sqee hs kqmmuez zkqssuj tckl kvuozqvu. omj cs klok toz mhk umhqnl shv sowu, kluvu toz oezh lcz yvhehmnuj pcnhqv kh wovpue ok. kcwu thvu hm, aqk ck zuuwuj kh lopu eckkeu ussudk hm wv. aonncmz. ok mcmukg lu toz wqdl klu zowu oz ok scskg. ok mcmukg-mcmu klug aunom kh doee lcw tuee-yvuzuvpuj; aqk qmdlomnuj thqej lopu auum muovuv klu wovr. kluvu tuvu zhwu klok zlhhr klucv luojz omj klhqnlk klcz toz khh wqdl hs o nhhj klcmn; ck zuuwuj qmsocv klok omghmu zlhqej yhzzuzz (oyyovumkeg) yuvyukqoe ghqkl oz tuee oz (vuyqkujeg) cmubloqzkcaeu tuoekl. ck tcee lopu kh au yocj shv, klug zocj. ck czmk mokqvoe, omj kvhqaeu tcee dhwu hs ck! aqk zh sov kvhqaeu loj mhk dhwu; omj oz wv. aonncmz toz numuvhqz tckl lcz whmug, whzk yuhyeu tuvu tceecmn kh shvncpu lcw lcz hjjckcuz omj lcz nhhj shvkqmu. lu vuwocmuj hm pczckcmn kuvwz tckl lcz vueokcpuz (ubduyk, hs dhqvzu, klu zodrpceeuaonncmzuz), omj lu loj womg juphkuj ojwcvuvz owhmn klu lhaackz hs yhhv omj qmcwyhvkomk sowcecuz. aqk lu loj mh dehzu svcumjz, qmkce zhwu hs lcz ghqmnuv dhqzcmz aunom kh nvht qy. klu uejuzk hs kluzu, omj aceahz sophqvcku, toz ghqmn svhjh aonncmz. tlum aceah toz mcmukg-mcmu lu ojhykuj svhjh oz lcz lucv, omj avhqnlk lcw kh ecpu ok aon umj; omj klu lhyuz hs klu zodrpceeu- aonncmzuz tuvu scmoeeg jozluj. aceah omj svhjh loyyumuj kh lopu klu zowu acvkljog, zuykuwauv 22mj. ghq loj aukkuv dhwu omj ecpu luvu, svhjh wg eoj, zocj aceah hmu jog; omj klum tu dom dueuavoku hqv acvkljog-yovkcuz dhwshvkoaeg khnukluv. ok klok kcwu svhjh toz zkcee cm lcz ktuumz, oz klu lhaackz doeeuj klu cvvuzyhmzcaeu ktumkcuz auktuum dlcejlhhj omj dhwcmn hs onu ok klcvkg-klvuu'
+ctext = re.sub('[^A-Z]','',ctext.upper())
+
+maxkey = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+maxscore = -99e9
+parentscore,parentkey = maxscore,maxkey[:]
+print ("Substitution Cipher solver, you may have to wait several iterations")
+print ("for the correct result. Press ctrl+c to exit program.")
+# keep going until we are killed by the user
+i = 0
+while 1:
+    i = i+1
+    random.shuffle(parentkey)
+    deciphered = SimpleSub(parentkey).decipher(ctext)
+    parentscore = fitness.score(deciphered)
+    count = 0
+    while count < 1000:
+        a = random.randint(0,25)
+        b = random.randint(0,25)
+        child = parentkey[:]
+        # swap two characters in the child
+        child[a],child[b] = child[b],child[a]
+        deciphered = SimpleSub(child).decipher(ctext)
+        score = fitness.score(deciphered)
+        # if the child was better, replace the parent with it
+        if score > parentscore:
+            parentscore = score
+            parentkey = child[:]
+            count = 0
+        count = count+1
+    # keep track of best score seen so far
+        if parentscore>maxscore:
+            maxscore,maxkey = parentscore,parentkey[:]
+            print ('\nbest score so far:',maxscore,'on iteration',i)
+            ss = SimpleSub(maxkey)
+            print( '    best key: '+''.join(maxkey))
+            print ('    plaintext: '+ss.decipher(ctext))
+
